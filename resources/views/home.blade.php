63314b86f0bff58e53436cf395787775f6b7809c
nolanmurphey <paTrick10!>
2019-10-17 17:38:39 -0400

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            test
+        </div>
+        <div class="col-9">
+            <div><h1>nolanmurphey</h1></div>
+        </div>
+    </div>
+</div>
+@endsection
