c6976438f2fe5612ca3c93cb621620627daae022
nolanmurphey <paTrick10!>
2019-10-18 16:21:21 -0400

    Database migrations for posts and profiles added, Homepage replaced with profile view page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-3">
-            test
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-5 pt-5" align ="center">
+                <img src="https://scontent-iad3-1.cdninstagram.com/vp/dd1c97d1ae921b1373e925e906bd26a0/5E2CE3E5/t51.2885-19/s150x150/20686955_269260230227541_8595081543368572928_a.jpg?_nc_ht=scontent-iad3-1.cdninstagram.com" class="rounded-circle">
+            </div>
+            <div class="col-6 pt-5">
+                <div class="d-flex justify-content-between align-items-baseline">
+                    <h1>{{ $user->username }}</h1>
+                    <a href="">Add New Post</a>
+                </div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>53</strong> posts</div>
+                    <div class="pr-5"><strong>160</strong> followers</div>
+                    <div class="pr-5"><strong>291</strong> following</div>
+                </div>
+                <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+                <div>{{ $user->profile->description }}</div>
+                <div><a href="#">{{ $user->profile->url }}</a></div>
+            </div>
         </div>
-        <div class="col-9">
-            <div><h1>nolanmurphey</h1></div>
+        <div class="row pt-5 justify-content-center">
+            <div class="col" align="right">
+                <img src="https://scontent-iad3-1.cdninstagram.com/vp/2be59a8d677469a63bf75b90e61d8a31/5E5F8C8B/t51.2885-15/sh0.08/e35/c0.180.1440.1440a/s640x640/69358796_168510640869091_7151890652198426599_n.jpg?_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=107" class="w-75">
+            </div>
+            <div class="col" align="center">
+                <img src="https://scontent-iad3-1.cdninstagram.com/vp/2be59a8d677469a63bf75b90e61d8a31/5E5F8C8B/t51.2885-15/sh0.08/e35/c0.180.1440.1440a/s640x640/69358796_168510640869091_7151890652198426599_n.jpg?_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=107" class="w-75">
+            </div>
+            <div class="col" align="left">
+                <img src="https://scontent-iad3-1.cdninstagram.com/vp/2be59a8d677469a63bf75b90e61d8a31/5E5F8C8B/t51.2885-15/sh0.08/e35/c0.180.1440.1440a/s640x640/69358796_168510640869091_7151890652198426599_n.jpg?_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=107" class="w-75">
+            </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,59 +15,52 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Insta</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        Insta
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        @auth
+                            <a class="nav-link" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sign out</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
                         @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                        <a class="nav-link" href="#">Login</a>
+                        @endauth
 
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                    </li>
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Register</a>
+                    </li>
+                    @endguest
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
             </div>
         </nav>
 
